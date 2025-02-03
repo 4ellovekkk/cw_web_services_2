9073ae05518909bd44ff314f8378b57a77876150
4ellovekkkk <mozgolomykow18@gmail.com>
2025-02-03 13:42:58 +0300

    created table for images and files && package updates

---
+++ server/sql/database_test.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE web_services_cw;
+
+GO
+;
+
+USE web_services_cw;
+
+-- Таблица ролей
+CREATE TABLE role
+(
+    id               INT IDENTITY (1, 1) PRIMARY KEY,
+    role_name        NVARCHAR(200),
+    role_privs_level INT
+);
+
+-- Таблица факультетов
+CREATE TABLE faculties
+(
+    id           INT IDENTITY (1, 1) PRIMARY KEY,
+    faculty_name NVARCHAR(200) unique
+);
+
+-- Таблица специальностей
+CREATE TABLE specialty
+(
+    id        INT IDENTITY (1, 1) PRIMARY KEY,
+    faculty   INT FOREIGN KEY REFERENCES faculties (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    spec_name NVARCHAR(200) unique
+);
+
+-- Общая таблица пользователей
+CREATE TABLE users
+(
+    id           INT IDENTITY (1, 1) PRIMARY KEY,
+    first_name   NVARCHAR(200),
+    midle_name   NVARCHAR(200),
+    last_name    NVARCHAR(200),
+    phone        NCHAR(15),
+    user_type    NVARCHAR(50),
+    -- Тип пользователя: 'worker' или 'student'
+    username     VARCHAR(200) UNIQUE NOT NULL,
+    passwd       VARCHAR(255),
+    user_role    INT FOREIGN KEY REFERENCES role (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    is_locked    BIT,
+    position     NVARCHAR(200),
+    is_dean      BIT,
+    specialty    INT FOREIGN KEY REFERENCES specialty (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    faculty      INT FOREIGN KEY REFERENCES faculties (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    group_number INT,
+    is_headman   BIT
+);
+
+-- Таблица типов документов
+CREATE TABLE document_types
+(
+    id                   INT IDENTITY (1, 1) PRIMARY KEY,
+    document_name        NVARCHAR(200),
+    is_payment_mandatory BIT
+);
+
+-- Таблица документов
+CREATE TABLE documents
+(
+    id               INT IDENTITY (1, 1) PRIMARY KEY,
+    -- SERIAL заменен на IDENTITY, так как это SQL Server
+    document_type_id INT           NOT NULL,
+    content          NVARCHAR(MAX) NOT NULL,
+    -- Используем NVARCHAR(MAX) вместо TEXT, так как TEXT устарел в SQL Server
+    created_at       DATETIME DEFAULT GETDATE(),
+    updated_at       DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (document_type_id) REFERENCES document_types (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    is_approved      BIT,
+    is_dean_required BIT
+);
+
+create table DocumentsProve
+(
+    id       int identity (1,1) primary key,
+    document varbinary(max) not null,
+);
